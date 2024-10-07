2bfc8967f16b6a5797aa47ca550fb1a46cffe473
hedesiustefan <stefan.hedesiu@gmail.com>
2024-10-07 16:33:53 +0300

    second task third and fourth part

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -1,10 +1,15 @@
 public class Aufgabe2 {
     public static void main(String[] args) {
         int[] numbers = {45, 12, 78, 36, 59, 23, 81, 7, 50, 91, 14, 67, 88, 32, 3, 62, 19, 96, 27, 41};
+        int[] numbers2 = {4, 8, 3, 10, 17};
         //Unterpunkt 1
         System.out.println(maxNumber(numbers));
         //UNterpunkt 2
         System.out.println(minNumber(numbers));
+        //Unterpunkt 3
+        System.out.println(maxSum(numbers2));
+        //UNterpunkt 4
+        System.out.println(minSum(numbers2));
     }
 
     public static int maxNumber(int[] numbers) {
@@ -25,5 +30,27 @@ public class Aufgabe2 {
             }
         }
         return min;
+    }
+
+    public static int maxSum(int[] numbers) {
+        int minNumber = minNumber(numbers);
+        int sum = 0;
+        for (int number : numbers) {
+            if (minNumber != number) {
+                sum += number;
+            }
+        }
+        return sum;
+    }
+
+    public static int minSum(int[] numbers) {
+        int maxNumber = maxNumber(numbers);
+        int sum = 0;
+        for (int number : numbers) {
+            if (maxNumber != number) {
+                sum += number;
+            }
+        }
+        return sum;
     }
 }

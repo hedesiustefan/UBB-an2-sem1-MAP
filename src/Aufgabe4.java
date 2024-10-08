7d5f645259556840273f632a41faaf553f2b35dc
hedesiustefan <stefan.hedesiu@gmail.com>
2024-10-08 15:37:53 +0300

    fourth task second part

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -4,17 +4,34 @@ public class Aufgabe4 {
         int[] drives = {10, 75, 80, 30, 25, 10, 60};
         //unterpunkt 1
         System.out.println("Most cheap keyboard is: " + cheapestKeyboard(keyboards));
+        //Unterpunkt 2
+        System.out.println("Most expensive thing is: " + mostExpensiveThing(keyboards, drives));
 
     }
 
     public static int cheapestKeyboard(int[] keyboards) {
-        int minPrice = 1000000;
+        int minPrice = Integer.MAX_VALUE;
         for (int keyboard : keyboards) {
             if (keyboard < minPrice) {
                 minPrice = keyboard;
             }
         }
         return minPrice;
+    }
+
+    public static int mostExpensiveThing(int[] keyboards, int[] drives) {
+        int maxPrice = Integer.MIN_VALUE;
+        for (int keyboard : keyboards) {
+            if (keyboard > maxPrice) {
+                maxPrice = keyboard;
+            }
+        }
+        for (int drive : drives) {
+            if (drive > maxPrice) {
+                maxPrice = drive;
+            }
+        }
+        return maxPrice;
     }
 
 }

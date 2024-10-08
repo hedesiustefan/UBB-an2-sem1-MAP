b4e6c86306020cde357ce047811f2025d9381ad4
hedesiustefan <stefan.hedesiu@gmail.com>
2024-10-08 15:41:44 +0300

    fourth task third part

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -2,10 +2,13 @@ public class Aufgabe4 {
     public static void main(String[] args) {
         int[] keyboards = {5, 15, 20, 35, 50, 65, 40};
         int[] drives = {10, 75, 80, 30, 25, 10, 60};
+        int budget = 35;
         //unterpunkt 1
         System.out.println("Most cheap keyboard is: " + cheapestKeyboard(keyboards));
         //Unterpunkt 2
         System.out.println("Most expensive thing is: " + mostExpensiveThing(keyboards, drives));
+        //Unterpunkt 3
+        System.out.println("Most expensive drive you can afford: " + driveAccessibleByBudget(budget, drives));
 
     }
 
@@ -28,6 +31,16 @@ public class Aufgabe4 {
         }
         for (int drive : drives) {
             if (drive > maxPrice) {
+                maxPrice = drive;
+            }
+        }
+        return maxPrice;
+    }
+
+    public static int driveAccessibleByBudget(int budget, int[] drives) {
+        int maxPrice = Integer.MIN_VALUE;
+        for (int drive : drives) {
+            if (drive > maxPrice && drive < budget) {
                 maxPrice = drive;
             }
         }
